5c2fe769dbe117836209ec8d604b975d95c3e41c
Phong-Ngu <55851543+Phong-Ngu@users.noreply.github.com>
2022-03-29 21:16:13 -0500

    Jason front end

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './App.css';
+import Searchbar from './components/Searchbar';
+import LoginButton from './components/loginButton';
+ // this is where everthing is render
+
+function App() {
+
+ return (
+  <div>
+    <header className="App"> 
+      <LoginButton />
+    </header>
+    <header className="App-header">
+      Three A Mangos
+    </header>
+    <body className="App-Search">
+      <Searchbar /> 
+    </body>
+    </div>
+ )
+}
+
+export default App;
