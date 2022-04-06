695d273fc8a4cef5b5b3815e361f07766916a881
Brandon Michaud <brandondmichaud@ou.edu>
2022-04-06 17:26:03 -0500

    moved react to client folder

---
+++ client/src/components/loginButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// Search bar for homepage
+
+ function loginButton(){
+    return (
+        <React.Fragment>
+            
+            <div >
+            <button className="login"> Login </button>
+
+            <button className="register"> Register </button>
+
+            </div>
+           
+         </React.Fragment>
+    )
+}
+
+export default loginButton;
