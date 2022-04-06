c477693ff20a63f00aedb6fcdc0e2d934debdf1c
Brandon Michaud <brandondmichaud@ou.edu>
2022-04-06 18:37:51 -0500

    Updated client

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import './App.css';
 import Searchbar from './components/Searchbar';
 import LoginButton from './components/loginButton';
  // this is where everthing is render
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { serverResponse: "" };
+  }
 
- return (
-  <div>
-    <header className="App"> 
-      <LoginButton />
-    </header>
-    <header className="App-header">
-      Three A Mangos
-    </header>
-    <body className="App-Search">
-      <Searchbar /> 
-    </body>
-    </div>
- )
+  callAPI() {
+    fetch("http://localhost:9000/testServer")
+      .then(res => res.text())
+      .then(res => this.setState({ serverResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+  render() {
+    return (
+      <div>
+        <header className="App"> 
+          <LoginButton />
+        </header>
+        <header className="App-header">
+          Three A Mangos
+        </header>
+        <body className="App-Search">
+          <Searchbar /> 
+        </body>
+        <p className="App-intro">;{this.state.serverResponse}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
