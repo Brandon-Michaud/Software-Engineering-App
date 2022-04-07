c38b34d28d8384dd9a50b0d78655d10620063922
Brandon Michaud <brandondmichaud@ou.edu>
2022-04-07 10:26:37 -0500

    Update changes to server

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testServerRouter = require('./routes/testServer')
+var testServerRouter = require('./routes/testServer.js')
 
 var app = express();
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', function(req, res, next) {
+  res.send('A JSON object of users');
 });
 
 module.exports = router;
