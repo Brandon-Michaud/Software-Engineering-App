d0d9edea9295355d97b9311ff70600f7b70ee283
Brandon Michaud <brandondmichaud@ou.edu>
2022-04-19 21:50:56 -0500

    Added log in component and renamed old login component to navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css'
+
+ function Navbar(){
+    return (
+        <div className='navbar'>
+            <button className="login"> Log In </button>
+
+            <button className="register"> Sign Up </button>
+
+        </div>
+    )
+}
+
+export default Navbar;
